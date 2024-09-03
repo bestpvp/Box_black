afeae9fcc035d03a219e694c88d04be00ad32be9
bestpvp <bestpvp@sina.com>
2024-09-03 21:36:28 +0800

    240904

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.java
@@ -38,23 +38,21 @@ public class AboutDialog extends BaseDialog {
         super(context);
         setContentView(R.layout.dialog_about);
 
-        TextView tv_title = findViewById(R.id.tv_title);
+        TextView tv_title = findViewById(R.id.tv_name);
         tv_title.setText(CustomUtil.getPrefix());
 
-        TextView tv_version = findViewById(R.id.tv_version);
+        TextView tv_version = findViewById(R.id.tv_title);
         tv_version.setText(CustomUtil.getTitle());
 
-        TextView tv_version_message = findViewById(R.id.tv_version_message);
-        tv_version_message.setText(CustomUtil.getAppMsg());
+        Button btnShowDate = findViewById(R.id.btn_show_version);
+        btnShowDate.setText("查看版本");
 
         String versionCode = String.valueOf(getVersionCode(context));
-
-        Button btnShowDate = findViewById(R.id.btn_show_version);
         btnShowDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Assuming AboutDialog is only used in MainActivity
                 Toast.makeText(getContext(), "当前版本: "+versionCode, Toast.LENGTH_SHORT).show();
+                dismiss();
             }
         });
     }
